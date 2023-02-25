ee4ee0ae911a8b74d122d7ac4d0e015c8b6ad3bd
AnasAA412 <Anasanchu4430@gmail.com>
2023-02-25 11:38:37 +0530

    project completed

--- user/views.py
+++ user/views.py
@@ -13,56 +13,56 @@ def menu(request):
     }
     return render(request, 'home.html',context=context)
 
-def login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            auth_login(request, user)
-            return redirect('/home')
-        else:
-            messages.success(request, ('successfully logging in'))
-            return redirect('/login')
-    else:
-        form = UserForm()
-        return render(request, 'user/login.html', {'form':form,})
-    
-
 # def login(request):
 #     if request.method == 'POST':
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-
-#         print(username)
-#         print(password)
-
-#         if username and password:
-#             user = authenticate(request, username=username, password=password)
-#             print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-            
-#             if user is not None:
-#                 auth_login(request, user)
-
-#                 return HttpResponseRedirect("/home")
-
+#         username = request.POST['username']
+#         password = request.POST['password']
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             auth_login(request, user)
+#             return redirect('/home')
 #         else:
-#             print("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
+#             messages.success(request, ('successfully logging in'))
+#             return redirect('/login')
+#     else:
+#         form = UserForm()
+#         return render(request, 'user/login.html', {'form':form,})
+    
 
-#         context = {
-#             "title" : "Login",
-#             "error": True,
-#             "message": "Invalid username or password"
-#         } 
-#         # print("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")   
-#         return render(request, "user/login.html", context=context)
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
-#     else: 
-#         print("cccccccccccccccccccccc")
-#         context = {
-#             "title": "Login",
-#         }    
-#         return render(request, "user/login.html", context=context)
+        print(username)
+        print(password)
+
+        if username and password:
+            user = authenticate(request, username=username, password=password)
+            print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+            
+            if user is not None:
+                auth_login(request, user)
+
+                return HttpResponseRedirect("/home")
+
+        else:
+            print("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
+
+        context = {
+            "title" : "Login",
+            "error": True,
+            "message": "Invalid username or password"
+        } 
+        # print("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")   
+        return render(request, "user/login.html", context=context)
+
+    else: 
+        print("cccccccccccccccccccccc")
+        context = {
+            "title": "Login",
+        }    
+        return render(request, "user/login.html", context=context)
 
 def logout(request):
     auth_logout(request)   
@@ -70,65 +70,65 @@ def logout(request):
 
 
 
-def signup(request):
-    if request.method == 'POST':
-        forms = UserForm(request.POST)
-        if forms.is_valid():
-            forms.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(username=username, password=password)
-            auth_login(request, user)
-            messages.success(request, ('Registration successfully'))
-            return redirect('/home')
-    else:
-        form = UserForm()
-    return render(request, "user/signup.html",  {
-        'form':form,
-    })
 # def signup(request):
-#     if request.method == "POST":
+#     if request.method == 'POST':
 #         forms = UserForm(request.POST)
 #         if forms.is_valid():
-#             instance = forms.save()
-
-#             user = User.objects.create_user(
-#                 name=instance.name,
-#                 username=instance.username,
-#                 password=instance.password,
-                
-#                 email=instance.email,
-                
-#             )
-
-            
-
-#             user = authenticate(request, username=instance.username, password=instance.password)
-#             auth_login(request,user)
-
-#             return HttpResponseRedirect("/home")
-
-#         else:
-#             message = generate_form_errors(forms)
-
-
-#             form = UserForm()
-#             context={
-            
-#             "title": "Signup ",
-#             "error": True,  
-#             "message": message,
-#             "form": form
-
-#             }    
-#             return render(request, "user/signup.html",context=context)
-
+#             forms.save()
+#             username = form.cleaned_data['username']
+#             password = form.cleaned_data['password']
+#             user = authenticate(username=username, password=password)
+#             auth_login(request, user)
+#             messages.success(request, ('Registration successfully'))
+#             return redirect('/home')
 #     else:
 #         form = UserForm()
-#         context={
-#             "title": "Signup", 
-#             "form": form
-#         }
-#         return render(request, "user/signup.html", context=context)
+#     return render(request, "user/signup.html",  {
+#         'form':form,
+#     })
+def signup(request):
+    if request.method == "POST":
+        forms = UserForm(request.POST)
+        if forms.is_valid():
+            instance = forms.save()
+
+            user = User.objects.create_user(
+                name=instance.name,
+                username=instance.username,
+                password=instance.password,
+                
+                email=instance.email,
+                
+            )
+
+            
+
+            user = authenticate(request, username=instance.username, password=instance.password)
+            auth_login(request,user)
+
+            return HttpResponseRedirect("/home")
+
+        else:
+            message = generate_form_errors(forms)
+
+
+            form = UserForm()
+            context={
+            
+            "title": "Signup ",
+            "error": True,  
+            "message": message,
+            "form": form
+
+            }    
+            return render(request, "user/signup.html",context=context)
+
+    else:
+        form = UserForm()
+        context={
+            "title": "Signup", 
+            "form": form
+        }
+        return render(request, "user/signup.html", context=context)
 
 
